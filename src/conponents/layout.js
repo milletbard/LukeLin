534df77553f4569df271b14cf580650670ee1da2
Luke Lin <milletbard@gmail.com>
2019-01-25 17:55:59 +0800

    first commit

---
+++ src/conponents/layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Home from '../conponents/Home';
+export default class layout extends Component {
+    render() {
+        return (
+            <div>
+                <Home />
+            </div>
+        )
+    }
+}
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,142 +1,15 @@
 import React, { Component } from 'react';
-import Navbar from '../conponents/Navbar/Navbar';
-import './App.css'
-import styled from 'styled-components';
+import Layout from '../conponents/layout';
 
-//import styled from 'styled-components';
 
 
 
 class App extends Component {
-  state = {
-    showBurger: false
-
-  }
-  toggleBurgerHandler = () => {
-    const doesShow = this.state.showBurger;
-    this.setState({ showBurger: !doesShow });
-
-  }
 
   render() {
-    const Div = styled.div`
-    height:500px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
 
-  `;
     return (
-      <div>
-        <Navbar
-          clicked={this.toggleBurgerHandler}
-          showBurger={this.state.showBurger}
-        />
-        <Div>
-
-        </Div>
-
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
+      <Layout />
     );
   }
 }
